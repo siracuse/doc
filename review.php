2ec89f4757bdf3d26a96c417e5957e4450d330e2
siracuse <siracuse.harichandra@gmail.com>
2020-05-29 13:35:47 +0200

    refactor review

--- review.php
+++ review.php
@@ -30,6 +30,7 @@
             <li>Supprimer les <code>parameters</code> inutiles dans config.yml</li>
             <li>Supprimer le fichier <code>todo.txt</code></li>
             <li>Revoir les routes <code>FOSUSer</code></li>
+            <li>Nommer le subject dans <code>config_prod.yml</code></li>
 
         </ul>
 
